f0aa461c6eedff67b1565aeb0d8267429c08b022
Carlos Lopez Z <krlosaren@gmail.com>
2020-09-12 10:29:04 -0300

    add csrf

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, request, make_response, redirect, render_template, sess
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from .models import UserModel
+from flask_wtf import CsrfProtect
 
 from .config import Config
 from .auth import auth
@@ -9,7 +10,7 @@ from .auth import auth
 
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
-
+csrf = CsrfProtect()
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -24,6 +25,7 @@ def create_app():
         ENV='development'
     )
 
+    csrf.init_app(app)
     login_manager.init_app(app)
 
     app.config.from_object(Config)
